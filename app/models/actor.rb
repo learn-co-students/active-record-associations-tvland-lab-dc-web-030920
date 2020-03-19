58088a66f87e14d656a04e6e52713272b5bc4203
lmonty22 <lindsay.montgomery@olympian.org>
2020-03-19 18:37:52 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+   
+    def list_roles
+        self.characters.map do |c|
+            "#{c.name} - #{Show.find_by(id:c.show_id).name}"
+        end
+    end 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters 
+
+def actors_list
+    characters = Character.all.select do |c|
+        c.show_id = self.id 
+    end
+    characters.map do |c|
+        Actor.find_by(id:c.actor_id).full_name
+    end
+end
+
 end
